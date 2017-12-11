85d8acdef5125f7fc2568d4038910a96f7bb53ca
C0L3NzO <kaspar.jakobson@khk.ee>
2017-12-11 12:41:36 +0200

    Lisatud muutujad

--- mina.php
+++ mina.php
@@ -14,3 +14,12 @@ $kaspar="Kaspar";
 echo "Minu eesnimi on ".$kaspar."<br>";
 print "Minu eesnimi on ".$kaspar."<br>";
 print("Minu eesnimi on $kaspar<br>");
+
+$var1 = 5;
+$var2 = 5.0;
+$var3 = "5";
+$var4 = 2;
+$var5 = 2.0;
+$var6 = "2";
+$var7 = true;
+$var8 = false;
