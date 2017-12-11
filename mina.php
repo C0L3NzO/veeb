02512e12186aec0863c78c378f7c09f10d6aa41c
C0L3NzO <kaspar.jakobson@khk.ee>
2017-12-11 12:38:11 +0200

    Väljastatud muutuja

---
+++ mina.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 11.12.17
+ * Time: 12:12
+ */
+//üherealine kommentaar
+/*
+mitmerealine
+*/
+
+$kaspar="Kaspar";
+echo "Minu eesnimi on ".$kaspar."<br>";
+print "Minu eesnimi on ".$kaspar."<br>";
+print("Minu eesnimi on $kaspar<br>");
