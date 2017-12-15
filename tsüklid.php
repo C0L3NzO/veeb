37ae00e2f737fe18374056b11657a38b5a0d66cd
C0L3NzO <kaspar.jakobson@khk.ee>
2017-12-15 11:02:47 +0200

    Continue kasutus

---
+++ tsüklid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaspar
+ * Date: 15.12.17
+ * Time: 10:30
+ */
+$varv = "";
+for($arv - 1; $arv <= 10; $arv++){
+    if ($arv % 2 == 0){
+        $varv = "red";
+    } else {
+        $varv = "blue";
+    }
+    echo '<p style="color: '.$varv.';">'.$arv."</p>";
+}
+$kord = 0;
+while($kord++ <= 4){
+    if($kord == 3) continue;
+    echo "<i>".$kord."</i>";
+}
