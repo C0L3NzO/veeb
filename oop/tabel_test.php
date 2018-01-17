1978cbd676be0170ed3afe10cb3a349f2782b1eb
C0L3NzO <kaspar.jakobson@khk.ee>
2018-01-17 10:38:42 +0200

    loodud pealkirjade järgi järjestatud tabeli funktsioon

--- oop/tabel_test.php
+++ oop/tabel_test.php
@@ -10,8 +10,9 @@ require_once "tabel.php";
 //loon tabeli objekti
 $minuTabel = new tabel(array("a", "b", "c", "d"));
 //lisame read juurde
-$minuTabel->lisaRida(array(1, 2, 3, 4));
-$minuTabel->lisaRida(array(5, 6, 7, 8));
+//$minuTabel->lisaRida(array(1, 2, 3, 4));
+//$minuTabel->lisaRida(array(5, 6, 7, 8));
+$minuTabel->lisaRidaPealkirjadega(array("d"=>1, "c"=>2, "b"=>3, "a"=>4));
 //väljastan tabeli objekti test kujul
 echo "<pre>";
 print_r($minuTabel);
